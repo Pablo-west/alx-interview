6da6cc3d5c04e37e1f0dedc85f079ec874572d75
Pablo West <84607278+Pablo-west@users.noreply.github.com>
2024-05-02 20:48:56 +0000

    Update 0-stats.py

--- 0x03-log_parsing/0-stats.py
+++ 0x03-log_parsing/0-stats.py
@@ -1,40 +1,37 @@
 #!/usr/bin/python3
-""" a script that reads stdin line by line and computes metrics """
-
-from sys import stdin
+'''a script that reads stdin line by line and computes metrics'''
 
 
-status_dict = {200: 0, 301: 0, 400: 0, 401: 0, 403: 0, 404: 0, 405: 0, 500: 0}
-total_file_size = 0
-count = 0
+import sys
 
-
-def print_all():
-    """function to print all"""
-    print("File size:", total_file_size)
-    for key, value in status_dict.items():
-        if value:
-            print("{}: {}".format(key, value))
-
+cache = {'200': 0, '301': 0, '400': 0, '401': 0,
+         '403': 0, '404': 0, '405': 0, '500': 0}
+total_size = 0
+counter = 0
 
 try:
-    for line in stdin:
-        count += 1
-        line = line.split()
-        try:
-            file_size = int(line[-1])
-            total_file_size += file_size
-        except (IndexError, ValueError, TypeError):
-            continue
-        try:
-            status_code = int(line[-2])
-            if status_code in status_dict.keys():
-                status_dict[status_code] += 1
-        except (IndexError, ValueError, TypeError):
-            continue
-        if count == 10:
-            print_all()
-            count = 0
-    print_all()
-except KeyboardInterrupt:
-    print_all()
+    for line in sys.stdin:
+        line_list = line.split(" ")
+        if len(line_list) > 4:
+            code = line_list[-2]
+            size = int(line_list[-1])
+            if code in cache.keys():
+                cache[code] += 1
+            total_size += size
+            counter += 1
+
+        if counter == 10:
+            counter = 0
+            print('File size: {}'.format(total_size))
+            for key, value in sorted(cache.items()):
+                if value != 0:
+                    print('{}: {}'.format(key, value))
+
+except Exception as err:
+    pass
+
+finally:
+    print('File size: {}'.format(total_size))
+    for key, value in sorted(cache.items()):
+        if value != 0:
+            print('{}: {}'.format(key, value))
